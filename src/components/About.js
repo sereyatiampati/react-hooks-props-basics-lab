8dac352aa9345ed986c2f75a026f49f98f599f98
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-01-12 01:23:26 +0300

    handing props and destructuring

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-function About() {
+function Links(props){
+
+  const {github, linkedin}= props
+  
+  return (
+     <div>
+      <h3>Links</h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+     </div>
+
+  )
+}
+
+function About(props) {
+  
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {props.bio && props.bio !== "" ? <p>{props.bio}</p>: null}
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQskVSdiLfFbCLHOzJCIMOM7K2cGIGN8lbTeg&usqp=CAU" alt="I made this" />
+      <Links github= {props.github} linkedin={props.linkedin}/>
     </div>
   );
 }
